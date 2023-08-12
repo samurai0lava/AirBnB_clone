944f2002ef5f6863a038948ecd54e6e04a952faa
Manalhub <manalrahali24@gmail.com>
2023-08-12 05:49:17 -0700

    fix pycodestyle test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,9 +5,11 @@ from models.base_model import BaseModel
 
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
 
     def test_init_with_kwargs(self):
+        d_f = '%Y-%m-%dT%H:%M:%S.%f'
         created_at = "2023-08-09T12:34:56.789012"
         updated_at = "2023-08-09T13:45:30.987654"
         obj_dict = {
@@ -18,21 +20,21 @@ class TestBaseModel(unittest.TestCase):
         obj = BaseModel(**obj_dict)
 
         self.assertEqual(obj.id, "test-id")
-        self.assertEqual(obj.created_at, datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%f'))
-        self.assertEqual(obj.updated_at, datetime.strptime(updated_at, '%Y-%m-%dT%H:%M:%S.%f'))
+        self.assertEqual(obj.created_at, datetime.strptime(created_at, d_f))
+        self.assertEqual(obj.updated_at, datetime.strptime(updated_at, d_f))
 
     def test_init_without_kwargs(self):
         obj = BaseModel()
         self.assertIsInstance(obj.id, str)
         self.assertIsInstance(obj.created_at, datetime)
         self.assertIsInstance(obj.updated_at, datetime)
-    
+
     def test_save(self):
         obj = BaseModel()
         prev_updated_at = obj.updated_at
         obj.save()
         self.assertNotEqual(prev_updated_at, obj.updated_at)
-    
+
     def test_to_dict(self):
         obj = BaseModel()
         obj_dict = obj.to_dict()
@@ -40,6 +42,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(obj_dict['id'], obj.id)
         self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
         self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
+
 
 if __name__ == '__main__':
     unittest.main()
