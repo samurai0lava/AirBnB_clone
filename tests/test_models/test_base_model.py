152fdbfa3a4b0f015a11a4f73ba720deb08c278c
Manalhub <manalrahali24@gmail.com>
2023-08-12 06:15:19 -0700

    add documentation

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+Unittest classe:
+    TestBaseModel
+"""
 import unittest
 import models
 from models.base_model import BaseModel
@@ -7,8 +11,13 @@ from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
 
     def test_init_with_kwargs(self):
+        """
+        Test instantiation of BaseModel with keyword arguments.
+
+        """
         d_f = '%Y-%m-%dT%H:%M:%S.%f'
         created_at = "2023-08-09T12:34:56.789012"
         updated_at = "2023-08-09T13:45:30.987654"
@@ -24,18 +33,30 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(obj.updated_at, datetime.strptime(updated_at, d_f))
 
     def test_init_without_kwargs(self):
+        """
+        Test instantiation of BaseModel without keyword arguments.
+
+        """
         obj = BaseModel()
         self.assertIsInstance(obj.id, str)
         self.assertIsInstance(obj.created_at, datetime)
         self.assertIsInstance(obj.updated_at, datetime)
 
     def test_save(self):
+        """
+        Test the 'save' method of BaseModel.
+
+        """
         obj = BaseModel()
         prev_updated_at = obj.updated_at
         obj.save()
         self.assertNotEqual(prev_updated_at, obj.updated_at)
 
     def test_to_dict(self):
+        """
+        Test the 'to_dict' method of BaseModel.
+
+        """
         obj = BaseModel()
         obj_dict = obj.to_dict()
         self.assertEqual(obj_dict['__class__'], 'BaseModel')
